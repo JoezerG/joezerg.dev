92d291d7873821610e8d0e715be2a71fecfda2e6
JoezerG <joezergullo@gmail.com>
2020-10-16 23:27:01 -0500

    underconstruction page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,87 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import LogoCyanWhite from "../images/cyan_white.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faInstagram } from "@fortawesome/free-brands-svg-icons"
+
+const MainContainer = styled.div`
+  background-color: #0a192f;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Logo = styled.img`
+  max-width: 420px;
+`
+
+const Skills = styled.ul`
+  width: 100%;
+  max-width: 220px;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+
+  li {
+    color: white;
+    font-size: 1.3rem;
+    margin-bottom: 1rem;
+    font-weight: 300;
+    line-height: 1.2;
+
+    a {
+      color: white;
+      font-weight: 400;
+    }
+
+    &:first-child {
+      font-weight: 400;
+    }
+  }
+`
+
+const UnderConstruction = styled.h2`
+  color: white;
+  font-weight: 700;
+  font-size: 2rem;
+`
+
+const Socials = styled.div`
+  margin-top: 1rem;
+`
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="En construcción" />
+    <MainContainer>
+      <UnderConstruction>
+        Sitio en <br /> construcción
+      </UnderConstruction>
+      <Logo src={LogoCyanWhite} />
+      <Skills>
+        <li>Joezer Gullo</li>
+        <li>
+          FullStack Developer @{" "}
+          <a
+            href="https://www.instagram.com/sonartelematicslatam/"
+            target="_blank"
+          >
+            SonarTelematics
+          </a>
+        </li>
+        {/* <li>Músico</li> */}
+      </Skills>
+      <Socials>
+        <a href="https://www.instagram.com/joezerg/" target="_blank">
+          <FontAwesomeIcon size="2x" color="white" icon={faInstagram} />
+        </a>
+      </Socials>
+    </MainContainer>
   </Layout>
 )
 
